c01642976c77da658d005ab014d6833204ee4274
Ingrid Seurin <ingridseurin@gmail.com>
2024-07-18 20:12:31 +0200

    Ajout meta description, minification des js et css, ajout adresse de Nina (html et css). Modif couleur filtre pour meilleur contraste. Correction du bug de la galerie, réduction taille images.

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -146,17 +146,17 @@
         });
       }
       let index = 0,
-        next = null;
+        prev = null;
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i ;
         }
       });
-      next =
-        imagesCollection[index] ||
+      prev =
+        imagesCollection[index-1] ||
         imagesCollection[imagesCollection.length - 1];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+      $(".lightboxImage").attr("src", $(prev).attr("src"));
     },
     nextImage() {
       let activeImage = null;
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index+1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
@@ -207,7 +207,7 @@
                                 ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
-                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
+                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clic"/>
                             ${
                               navigation
                                 ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['index.html'],
+    css: ['./assets/bootstrap/brut/bootstrap.css'],
+    keyframes : true,
+    output:'./assets/bootstrap/purified/bootstrap.css',
+    rejected: true,
+    variables: true
+  }
